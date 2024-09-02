6d8f208148cdf4731b01fc7248e5721102c442a1
Trong <spektralfrogadier@gmail.com>
2024-09-02 08:52:25 -0700

    ThePirateBay update
    NEW HOST;
    ThePirateShip NixOS configuration + Sleepy-DWM = TODO

---
+++ homes/x86_64-linux/trong@ThePirateBay/default.nix
@@ -0,0 +1,54 @@
+{
+  config,
+  lib,
+  pkgs,
+  osConfig,
+  namespace,
+  ...
+}:
+with lib;
+with lib.${namespace}; {
+  snowfallorg.user = {
+    enable = true;
+    name = "trong";
+  };
+
+  # Shell Management
+  shells = {
+    zsh.enable = true;
+    starship.enable = true;
+  };
+
+  # Applications
+  apps = {
+    misc = {
+      enable = true;
+      terms.foot.enable = true;
+      spicetify.enable = true;
+    };
+
+    programming = {
+      enable = true;
+      nixvim.enable = true;
+      gamedev.enable = true;
+    };
+
+    gaming = {
+      enable = true;
+
+      extras = {
+        proton.enable = false;
+        #minecraft.enable = true;
+      };
+    };
+  };
+
+  # Theming
+  themes = {
+    gtk.palenight.enable = true;
+    icons.papirus.enable = true;
+    cursors.numix.enable = true;
+  };
+
+  home.stateVersion = lib.mkDefault (osConfig.system.stateVersion or "24.05");
+}
---
+++ systems/x86_64-linux/ThePirateBay/default.nix
@@ -0,0 +1,80 @@
+# ThePirateBay - The Oceanic Desktop
+{pkgs, ...}: {
+  imports = [ ./hardware.nix ];
+
+  # NixOS boot options
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";
+    useOSProber = true;
+  };
+
+  # Desktop hostname
+  networking.hostName = "ThePirateBay";
+
+  # Create user "trong" & set default shell
+  users.trong.create = true; 
+  users.users.trong.shell = pkgs.zsh;
+
+  # Set OS shell to Zsh
+  programs.zsh.enable = true;
+
+  # Nix options
+  nix = {
+    gc = { # Automatic garbage collection
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+  };
+
+  # Different suites for different needs 
+  suites = {
+    common.enable = true; # Enables the common desktop properties
+    gaming.enable = true; # Gaming options
+  };
+
+  # Enables the Oceanic desktop modules
+  oceanic = {
+    utils.enable = true;
+    audio.enable = true;
+    fonts.enable = true;
+    # TODO make a global 'env' module for environment variables etc
+  };
+
+  # SDDM prerequisite
+  environment.systemPackages = [(
+    pkgs.catppuccin-sddm.override {
+      flavor = "mocha";
+      font  = "Comic Mono";
+      fontSize = "12";
+      background = "${./backgrounds/TPS.png}";
+      loginBackground = true;
+    }
+  )];
+
+  # Desktop environment
+  services = {
+    xserver = {
+      enable = true;
+      updateDbusEnvironment = true;
+      videoDrivers = [ "intel-media-driver" ];
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+    };
+    
+    displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+
+      theme = "catppuccin-mocha";
+      package = pkgs.kdePackages.sddm;
+    };
+  };
+
+  desktops.hyprland.enable = true;
+
+  system.stateVersion = "24.05";
+}
